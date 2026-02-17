fcdd5640a12a5a2ef046462f1c230e9f222a4236
Eric Sigurdson <eric@ericsigurdson.com>
2026-02-17 16:26:32 -0500

    feat: enable nvidia drivers and hardware graphics

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -29,6 +29,44 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.graphics = {
+    enable = true;
+  };
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # nouveau open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/Nvidia/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 535.43.02+
+    # Currently alpha-quality/experimental on GeForce cards.
+    open = true;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   # Enable the i3 window manager.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.windowManager.i3 = {
