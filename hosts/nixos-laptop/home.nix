5f8988dde3f84ac07f97c3bb33ade3aae0aa4cdc
Eric Sigurdson <eric@ericsigurdson.com>
2026-02-26 11:18:50 -0500

    refactor(home): centralize common configuration and add laptop monitor setup

--- hosts/nixos-laptop/home.nix
+++ hosts/nixos-laptop/home.nix
@@ -1,88 +1,17 @@
-{ config, pkgs, user, local, ... }:
+{ lib, ... }:
 
 {
   imports = [
-    ../../modules/home/zsh.nix
-    ../../modules/home/packages.nix
-    ../../modules/home/i3.nix
-    ../../modules/home/tmux.nix
-    ../../modules/home/neovim.nix
-    ../../modules/home/ssh.nix
+    ../../modules/home/default.nix
   ];
 
-  home.username = user;
-  home.homeDirectory = "/home/${user}";
-  home.stateVersion = "25.11";
-
-
-  # Individual user packages can go here
-  home.packages = with pkgs; [
-    # ripgrep
-    # fd
+  # Laptop-specific user settings
+  xsession.windowManager.i3.config.startup = [
+    # Monitor setup: Set external monitor (DP-2-1) to the left of laptop screen (eDP-1)
+    { 
+      command = "xrandr --output DP-2-1 --auto --left-of eDP-1";
+      always = true; 
+      notification = false; 
+    }
   ];
-
-  programs.home-manager.enable = true;
-
-  # Integrated programs
-  programs.firefox.enable = true;
-  programs.chromium = {
-	enable = true;
-	extensions = [
-		{ id = "hdokiejnpimakedhajhdlcegeplioahd"; } # lastpass
-	];
-   };
-
-  programs.git = {
-    enable = true;
-    settings = {
-      init.defaultBranch = "main";
-    };
-    includes = [
-      {
-        condition = "gitdir:${config.home.homeDirectory}/repos/work/";
-        contents = {
-          user = {
-            name = local.work.name;
-            email = local.work.email;
-          };
-        };
-      }
-      {
-        condition = "gitdir:${config.home.homeDirectory}/repos/personal/";
-        contents = {
-          user = {
-            name = local.personal.name;
-            email = local.personal.email;
-          };
-        };
-      }
-    ];
-  };
-
-  # Dark mode preference
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
-
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk";
-    style.name = "adwaita-dark";
-  };
 }
